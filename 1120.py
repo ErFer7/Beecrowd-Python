3b144f7c204e82cd1d980d0db97e6dafd4dc7cac
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2020-10-21 13:06:37 -0300

    Problema 1120 resolvido

---
+++ 1120.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+while True:
+
+    d, n = map(str, input().split())
+
+    if d == '0' and n == '0':
+        break
+
+    vStr = n.replace(d, '')
+
+    if vStr == '':
+
+        vStr = '0'
+    
+    print(int(vStr))
