2b13139e0e71b94994980ae71d654b80af9a22e6
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2020-11-05 20:51:42 -0300

    Problema 1185 resolvido

---
+++ 1185.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+res = 0
+
+o = input()
+
+mat = [None] * 12
+for i in range(12):
+
+    mat[i] = [None] * 12
+
+for i in range(12):
+
+    for j in range(12):
+
+        mat[i][j] = float(input())
+
+if o == "S":
+
+    for i in range(11):
+
+        for j in range(11 - i):
+
+            res += mat[i][j]
+else:
+
+    for i in range(11):
+
+        for j in range(11 - i):
+
+            res += mat[i][j] / 66.0
+
+print("{:.1f}".format(res))
