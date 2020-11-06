f2a6b406b5d6b6252e2b3c9a6807a5f9408d8453
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2020-11-05 21:12:52 -0300

    Problema 1186 resolvido

---
+++ 1186.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+res = 0
+
+o = input()
+
+mat = [None] * 12
+for i in range(12):
+
+    mat[i] = [None] * 12
+
+for i in range(12):
+
+    for j in range(12):
+
+        mat[i][j] = float(input())
+
+if o == "S":
+
+    for i in range(1, 12):
+
+        for j in range(12 - i, 12):
+
+            res += mat[i][j]
+else:
+
+    for i in range(1, 12):
+
+        for j in range(12 - i, 12):
+
+            res += mat[i][j] / 66.0
+
+print("{:.1f}".format(res))
